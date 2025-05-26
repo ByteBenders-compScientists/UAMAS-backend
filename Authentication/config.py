9821fdb2f065435e9921b36007700a0d222a1a79
lewmas9152 <smnjuguna22@gmail.com>
2025-05-26 15:12:39 +0300

    set cookies to be used for authorisation

--- Authentication/config.py
+++ Authentication/config.py
@@ -16,6 +16,10 @@ class Config:
     # JWT Settings
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=15)
-    JWT_TOKEN_LOCATION = ['headers', 'cookies']
+    JWT_TOKEN_LOCATION = ['cookies']
+    JWT_ACCESS_COOKIE_PATH = "/"
+    JWT_REFRESH_COOKIE_PATH = "/api/v1/auth/refresh"
+    JWT_COOKIE_SECURE = False  # True in production (only over HTTPS)
+    JWT_COOKIE_CSRF_PROTECT = False  # Prevent CSRF
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
