b5f575ca5391745a58adab11daa34738906ab0d7
Walter <126944679+waltertaya@users.noreply.github.com>
2025-06-28 17:22:16 +0300

    Update app.py

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -23,7 +23,7 @@ CORS(
     app,
     resources={
         r"/api/v1/*": {
-            "origins": ["*"],
+            "origins": [os.getenv('ORIGINS_URL'),'http://localhost:3000'],
             "methods": ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
             "allow_headers": ["Authorization","Content-Type"],
             "supports_credentials": True
